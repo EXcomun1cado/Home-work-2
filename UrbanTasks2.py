eebab9db04c6270418c9cc0e620dcf62e820a6d2
EXcomun1cado <s.ismoil0818@gmail.com>
2024-07-24 12:41:07 +0300

    Add files via upload

---
+++ UrbanTasks2.py
@@ -0,0 +1,5 @@
+home_work = 12
+time = 1.5
+course = 'Python'
+lesson = time / home_work
+print(f'Курс: {course}, всего задач: {home_work}, затрачено часов: {time}, среднее время выполнения: {lesson} часа.')
